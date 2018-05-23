ed5f1be8efae48cce3d3114c14c76a6bbff7e16d
郑江荣 <362675035@qq.com>
2018-05-23 13:43:28 +0800

    update

--- src/main/java/com/farwolf/updater/util/VResult.java
+++ src/main/java/com/farwolf/updater/util/VResult.java
@@ -2,8 +2,12 @@ package com.farwolf.updater.util;
 
 import org.jooq.impl.UpdatableRecordImpl;
 import org.jooq.tools.json.JSONObject;
+import org.jooq.util.derby.sys.Sys;
 
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zhengjiangrong on 17/5/27.
@@ -69,6 +73,29 @@ public class VResult extends JSONObject {
 
 
 
+    public static void main(String arg[])
+    {
+        ThreadPoolExecutor ex=new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        ex.getQueue().add(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("1111");
+            }
+        });
+        ex.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("ok");
+            }
+        });
+        ex.shutdown();
+
+
+
+
+    }
 
 
 
